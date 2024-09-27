95c9ab88d47e3e9514e00744d9d291e709c53d37
Guptan0506 <navyag0509@gmail.com>
2024-09-27 16:12:02 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Enter your age: ");
+        Scanner in = new Scanner(System.in);
+        int age;
+        age = in.nextInt();
+        if (age >= 21){
+            System.out.println("You get a wrist band");
+        }
+        else {
+
+        }
+    }
+}
